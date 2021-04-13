cd67748d8f40c4dc6d03f57bbc194299f7e00f13
jhon-martinez <jmartinez7083@misena.edu.co>
2021-04-12 20:55:19 -0500

    app estudiante

---
+++ Estudiante.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Evidencia._Estudiantes
+{
+    class Estudiante
+    {
+        public int Codigo { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Correo { get; set; }
+
+        public int Nota1 { get; set; }
+
+        public int Nota2 { get; set; }
+
+        public int Nota3 { get; set; }
+
+
+        public int TotalN(List<int> sumaNotas)
+
+        {
+            int suma = 0;
+
+            foreach (int SumaNota in sumaNotas)
+
+            {
+                suma = suma + SumaNota;
+            }
+
+            return suma;
+
+        }
+        public double PromedioN(int TotalN)
+
+        {
+            double Pro = TotalN / 100;
+            return Pro;
+
+
+        }
+
+
+
+    }
+
+
+
+}
